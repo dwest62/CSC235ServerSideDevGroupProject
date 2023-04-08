9aa7a1ba40aa0c6d7b3c6ce9e3feb5f8ac41358b
daemon <daemon@ip-172-26-12-159.ec2.internal>
2023-04-08 18:50:40 +0000

    t

--- updateServer.php
+++ updateServer.php
@@ -1,14 +1,14 @@
 
 <?php
+    echo "yep";
     $output = shell_exec('git checkout master 2>&1');
     echo "$output<br/>";
     $output = shell_exec('git pull 2>&1');
     echo "$output<br/>";
     $output = shell_exec('git checkout prod 2>&1');
     echo "$output<br/>";
-    $output = shell_exec('git merge master 2>&1');
+    $output = shell_exec('git merge master -m "merged" 2>&1');
     echo "$output<br/>";
     $output = shell_exec('git push 2>&1');
     echo "$output<br/>";
->>>>>>> master
 ?>
