a865c0e2d527914a6ade28c447f7a7ee59029f22
Debian <bitnami@ip-172-26-12-159.ec2.internal>
2023-04-08 13:16:56 +0000

    test shell

--- updateServer.php
+++ updateServer.php
@@ -1,1 +1,13 @@
-<?php `git pull`; ?>
+
+<?php
+    $output = shell_exec('git checkout master');
+    echo "$output";
+    $output = shell_exec('git pull');
+    echo "$output";
+    $output = shell_exec('git checkout prod');
+    echo "$output";
+    $output = shell_exec('git merge master');
+    echo "$output";
+    $output = shell_exec('git push');
+    echo "$output";
+?>
