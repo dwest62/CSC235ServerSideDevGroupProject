d2467c5164259a38fb98a0a078843d365f5c5825
James West <dwest62@gmail.com>
2023-04-08 18:14:27 +0000

    s

--- updateServer.php
+++ updateServer.php
@@ -1,11 +1,11 @@
 
 <?php
-    $output = shell_exec('git checkout master');
+    $output = shell_exec('git checkout master 2>&1');
     echo "$output<br/>";
     $output = shell_exec('git pull');
-    echo "$output<br/>";
-    $output = shell_exec('git checkout prod');
-    echo "$output<br/>";
-    $output = shell_exec('git merge master');
+    // echo "$output<br/>";
+    // $output = shell_exec('git checkout prod');
+    // echo "$output<br/>";
+    // $output = shell_exec('git merge master');
     echo "$output<br/>";
 ?>
